a97632cb6ebb1a482fb8570ce6238cf0f17bee4b
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2023-11-11 14:43:40 +0530

    v1.1.3: refactored vercel and package jsons

---
+++ dist/db/connection.js
@@ -0,0 +1,29 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.connect = void 0;
+const pg_1 = require("pg");
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new pg_1.Client({
+            user: process.env.POSTGRES_USER,
+            host: process.env.POSTGRES_HOST,
+            database: process.env.POSTGRES_DB,
+            password: process.env.POSTGRES_PASSWORD,
+            port: process.env.POSTGRES_PORT,
+            connectionString: process.env.POSTGRES_URL,
+        });
+        yield client.connect();
+        return client;
+    });
+}
+exports.connect = connect;
+//# sourceMappingURL=connection.js.map
